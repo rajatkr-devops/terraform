df58af6e74c37abfe58ccb63f21346f0006dc116
rajat <rajat.kumar.ghosh@accenture.com>
2022-05-09 20:50:42 +0530

    adding ec2 intance

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,28 @@
 resource "aws_instance" "testinstance"{
-    ami = ""
+    ami = "ami-09d56f8956ab235b3"
     instance_type = "t3.micro"
-    subnet_id = ""
-    assoicate_public_ip_address "true"
-    vpc_security_group_ids = ""
+    subnet_id = aws_subnet.public-subnet-1.id
+    associate_public_ip_address = true
+    vpc_security_group_ids = aws_security_group.ec2-aws-security.id
+    key_name = "test"
 }
+
+resource "aws_security_group" "ec2-aws-security" {
+    vpc_id = "${aws_vpc.test-env.id}"
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+// Terraform removes the default rule
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+  
